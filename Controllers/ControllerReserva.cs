810d439076c8788a25699e7437bc504bb9652f46
Alejo Párraga <alejoparragaitec@gmail.com>
2025-07-26 10:52:22 -0300

    CRUD de controllerReserva

--- Controllers/ControllerReserva.cs
+++ Controllers/ControllerReserva.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tito_s_Hotel.DAOs;
+using Tito_s_Hotel.Models;
 
 namespace Tito_s_Hotel.Controllers
 {
-    internal class ControllerReserva
+    public class ControllerReserva
     {
+        private DaoReserva oDaoReserva;
+        public ControllerReserva()
+        {
+            oDaoReserva = new DaoReserva;
+        }
+
+        public void crear(int numReserva, DateTime checkIn, DateTime checkOut, float adelanto, Habitacion oHabitacion, Pasajero oPasajero)
+        {
+            Reserva oReserva = new Reserva(numReserva, checkIn, checkOut, adelanto, oHabitacion, oPasajero);
+            oDaoReserva.crear(oReserva);
+        }
+        public void modificar(int numReserva, DateTime checkIn, DateTime checkOut, float adelanto, Habitacion oHabitacion, Pasajero oPasajero)
+        {
+            Reserva oReserva = new Reserva(numReserva, checkIn, checkOut, adelanto, oHabitacion, oPasajero);
+            oDaoReserva.modificar(oReserva);
+        }
+        public void eliminar(int numReserva, DateTime checkIn, DateTime checkOut, float adelanto, Habitacion oHabitacion, Pasajero oPasajero)
+        {
+            Reserva oReserva = new Reserva(numReserva, checkIn, checkOut, adelanto, oHabitacion, oPasajero);
+            oDaoReserva.eliminar(oReserva);
+        }
+        public void buscarTodasLasReservas(int numReserva, DateTime checkIn, DateTime checkOut, float adelanto, Habitacion oHabitacion, Pasajero oPasajero)
+        {
+            Reserva oReserva = new Reserva(numReserva, checkIn, checkOut, adelanto, oHabitacion, oPasajero);
+            oDaoReserva.BuscarTodasLasReservas(oReserva);
+        }
+
     }
 }
