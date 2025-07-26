fd04aeb57be95940643ea534af0fcc3824913e02
Alejo Párraga <alejoparragaitec@gmail.com>
2025-07-25 23:08:54 -0300

    CRUD en controllerHabitacion y controllerPasajero

--- Controllers/ControllerHabitacion.cs
+++ Controllers/ControllerHabitacion.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tito_s_Hotel.DAOs;
+using Tito_s_Hotel.Models;
 
 namespace Tito_s_Hotel.Controllers
 {
-    internal class ControllerHabitacion
+    public class ControllerHabitacion
     {
+        private DaoHabitacion oDaoHabitacion;
+        public ControllerHabitacion()
+        {
+            oDaoHabitacion = new DaoHabitacion();
+        }
+
+        public void crear(int numero, int camasSingle, bool camaDoble, float precio)
+        {
+            Habitacion oHabitacion = new Habitacion(numero, camasSingle, camaDoble, precio);
+            oDaoHabitacion.crear(oHabitacion);
+        }
+        public void modificar(int numero, int camasSingle, bool camaDoble, float precio)
+        {
+            Habitacion oHabitacion = new Habitacion(numero, camasSingle, camaDoble, precio);
+            oDaoHabitacion.modificar(oHabitacion);
+        }
+        public void eliminar(int numero, int camasSingle, bool camaDoble, float precio)
+        {
+            Habitacion oHabitacion = new Habitacion(numero, camasSingle, camaDoble, precio);
+            oDaoHabitacion.eliminar(oHabitacion);
+        }
+        public void buscarTodasLashabitaciones(int numero, int camasSingle, bool camaDoble, float precio)
+        {
+            Habitacion oHabitacion = new Habitacion(numero, camasSingle, camaDoble, precio);
+            oDaoHabitacion.buscarTodasLashabitaciones(oHabitacion);
+        }
+
     }
 }
--- Controllers/ControllerPasajero.cs
+++ Controllers/ControllerPasajero.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tito_s_Hotel.DAOs;
+using Tito_s_Hotel.Models;
 
 namespace Tito_s_Hotel.Controllers
 {
-    internal class ControllerPasajero
+    public class ControllerPasajero
     {
+        private DaoPasajero oDaoPasajero;
+        public ControllerPasajero()
+        {
+            oDaoPasajero = new DaoPasajero;
+        }
+
+        public void crear(string nombre, string apellido, int telefono, int dni, string correo)
+        {
+            Pasajero oPasajero = new Pasajero (nombre, apellido, telefono, dni, correo);
+            oDaoPasajero.crear(oPasajero);
+        }
+        public void modificar(string nombre, string apellido, int telefono, int dni, string correo)
+        {
+            Pasajero oPasajero = new Pasajero(nombre, apellido, telefono, dni, correo);
+            oDaoPasajero.modificar(oPasajero);
+        }
+        public void eliminar(string nombre, string apellido, int telefono, int dni, string correo)
+        {
+            Pasajero oPasajero = new Pasajero(nombre, apellido, telefono, dni, correo);
+            oDaoPasajero.eliminar(oPasajero);
+        }
+        public void buscarTodasLosPasajeros(string nombre, string apellido, int telefono, int dni, string correo)
+        {
+            Pasajero oPasajero = new Pasajero(nombre, apellido, telefono, dni, correo);
+            oDaoPasajero.buscarTodosLosPasajeros(oPasajero);
+        }
+
     }
 }
