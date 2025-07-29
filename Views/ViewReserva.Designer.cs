0422a2c834247ea68251c123ab538d5babb52e6c
Alejo Párraga <alejoparragaitec@gmail.com>
2025-07-28 23:48:56 -0300

    Se agrega ventana para filtrar busqueda por capacidad y periodo

--- Views/ViewReserva.Designer.cs
+++ Views/ViewReserva.Designer.cs
@@ -30,12 +30,14 @@
         {
             comboBox1 = new ComboBox();
             button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(148, 130);
+            comboBox1.Location = new Point(217, 124);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(121, 23);
             comboBox1.TabIndex = 0;
@@ -43,28 +45,51 @@
             // 
             // button1
             // 
-            button1.Location = new Point(148, 218);
+            button1.Location = new Point(86, 220);
             button1.Name = "button1";
             button1.Size = new Size(121, 23);
             button1.TabIndex = 1;
             button1.Text = "Nueva reserva";
             button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(296, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Tito´s Hotel";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(394, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Ver disponibilidad";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // ViewReserva
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(comboBox1);
             Name = "ViewReserva";
             Text = "ViewReserva";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComboBox comboBox1;
         private Button button1;
+        private Label label1;
+        private Button button2;
     }
 }
---
+++ filtrarDisponibilidadCapacidadPeriodo.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Tito_s_Hotel
+{
+    partial class filtrarDisponibilidadCapacidadPeriodo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Capacidad:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Check In:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(37, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(46, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Check Out:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(108, 94);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(210, 23);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(118, 141);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(212, 23);
+            dateTimePicker2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(152, 192);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // filtrarDisponibilidadCapacidadPeriodo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(385, 227);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "filtrarDisponibilidadCapacidadPeriodo";
+            Text = "filtrarDisponibilidadCapacidadPeriodo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Button button1;
+    }
+}
